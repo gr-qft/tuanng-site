de4be455a9abd3dafb874e768416bbcdceb90d88
Tuan Nguyen <69383359+gr-qft@users.noreply.github.com>
2021-04-26 09:26:50 -0400

    Use react-flip-toolkit

--- components/Interests.jsx
+++ components/Interests.jsx
@@ -1,58 +1,53 @@
+import { useState, useEffect } from "react";
+import shuffle from "lodash.shuffle";
+import { Flipper, Flipped } from "react-flip-toolkit";
 import styles from "./Interests.module.css";
 import { IconContext } from "react-icons";
-import { RiRunLine } from "react-icons/ri";
 import { FaBookOpen } from "react-icons/fa";
-import { GiCommercialAirplane } from "react-icons/gi";
-import { GiFruitBowl } from "react-icons/gi";
-import { RiMovie2Line } from "react-icons/ri";
-import { GiMusicalNotes } from "react-icons/gi";
-import { GiForestCamp } from "react-icons/gi";
+import { RiMovie2Line, RiRunLine } from "react-icons/ri";
+import {
+  GiMusicalNotes,
+  GiForestCamp,
+  GiFruitBowl,
+  GiCommercialAirplane,
+} from "react-icons/gi";
 import { BiSwim } from "react-icons/bi";
 import { HiOutlinePencil } from "react-icons/hi";
 
-export default function Interests() {
-  return (
-    <div className={styles.container}>
-      <IconContext.Provider
-        value={{ size: "2.5em", style: { verticalAlign: "middle" } }}
-      >
-        <div>
-          {" "}
-          <FaBookOpen />{" "}
-        </div>
-        <div>
-          <HiOutlinePencil />
-        </div>
-        <div>
-          {" "}
-          <RiRunLine />{" "}
-        </div>
+const data = {
+  reading: <FaBookOpen />,
+  writing: <HiOutlinePencil />,
+  runnning: <RiRunLine />,
+  travel: <GiCommercialAirplane />,
+  eating: <GiFruitBowl />,
+  movies: <RiMovie2Line />,
+  music: <GiMusicalNotes />,
+  outdoor: <GiForestCamp />,
+  swimming: <BiSwim />,
+};
 
-        <div>
-          {" "}
-          <GiCommercialAirplane />{" "}
-        </div>
-        <div>
-          {" "}
-          <GiFruitBowl />{" "}
-        </div>
-        <div>
-          {" "}
-          <RiMovie2Line />{" "}
-        </div>
-        <div>
-          {" "}
-          <GiMusicalNotes />{" "}
-        </div>
-        <div>
-          {" "}
-          <GiForestCamp />{" "}
-        </div>
-        <div>
-          {" "}
-          <BiSwim />{" "}
-        </div>
-      </IconContext.Provider>
-    </div>
+export default function Interests() {
+  const [items, setItems] = useState(Object.keys(data));
+  useEffect(() => {
+    const t = setInterval(() => setItems(shuffle), 2000);
+    return () => clearInterval(t);
+  }, []);
+
+  return (
+    <Flipper flipKey={items.join("")}>
+      <div className={styles.container}>
+        <IconContext.Provider
+          value={{ size: "2.5em", style: { verticalAlign: "middle" } }}
+        >
+          {items.map((item) => {
+            return (
+              <Flipped key={item} flipId={item}>
+                <div> {data[item]}</div>
+              </Flipped>
+            );
+          })}
+        </IconContext.Provider>
+      </div>
+    </Flipper>
   );
 }
