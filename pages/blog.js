9a7a53e15f456819f0af7f99059ee8aa05c4b14a
Tuan Nguyen <69383359+gr-qft@users.noreply.github.com>
2021-04-30 17:06:43 -0400

    Add info for blog

--- pages/blog.js
+++ pages/blog.js
@@ -3,7 +3,11 @@ import styles from "@/styles/Blog.module.css";
 export default function Blog() {
   return (
     <div className={styles.container}>
-      <h1> Blog </h1>
+      Under construction. Read about the plan on my{" "}
+      <a style={{ color: "red" }} href="https://github.com/gr-qft/tuanng-site">
+        Github repo
+      </a>
+      .
     </div>
   );
 }
